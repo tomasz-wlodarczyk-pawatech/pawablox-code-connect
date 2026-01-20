f15496f61870c1cf52a8db7e20fa07c5599f67c8
tomasz-wlodarczyk-pawatech <174312955+tomasz-wlodarczyk-pawatech@users.noreply.github.com>
2026-01-20 13:47:28 +0000

    feat(icons): regenerate icons from Figma

---
+++ packages/icons/src/icons/Icon2up.tsx
@@ -0,0 +1,16 @@
+// Auto-generated from Figma - DO NOT EDIT
+import React from 'react';
+import { Icon, IconProps } from '../Icon';
+
+export const Icon2up: React.FC<Omit<IconProps, 'children'>> = (props) => (
+  <Icon {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM7.22461 9.59473V11.4111H9.7998L9.94727 10.1416L11.5049 10.3818L11.6924 11.248L7.31934 14.7422L7.50684 17.3359H14.2715L14.4619 14.9697L10.626 15.0186L14.5566 11.9307L13.7852 8.62402L10.1533 7.37012L7.22461 9.59473ZM10.6758 6.82617L14.3975 8.08301L15.1963 11.4873L16.7754 10.1602L16.4316 6.41699L12.4453 5.48926L10.6758 6.82617Z"
+      fill="currentColor"
+    />
+  </Icon>
+);
+
+Icon2up.displayName = 'Icon2up';
---
+++ packages/icons/src/icons/IconPawa6.tsx
@@ -0,0 +1,18 @@
+// Auto-generated from Figma - DO NOT EDIT
+import React from 'react';
+import { Icon, IconProps } from '../Icon';
+
+export const IconPawa6: React.FC<Omit<IconProps, 'children'>> = (props) => (
+  <Icon {...props}>
+    <path
+      d="M14.0464 2L14.8564 5.00859L10.9371 6.06495L8.60597 15.9754L5.79895 14.3162L8.32975 3.54069L14.0464 2Z"
+      fill="currentColor"
+    />
+    <path
+      d="M11.3291 9.85554L17.1074 11.0733L18.2011 17.5282L11.932 22L7.86049 19.234L8.60797 15.9763L11.8881 18.2038L14.8034 16.1247L14.389 13.6844L10.6862 12.9043L11.3291 9.85554Z"
+      fill="currentColor"
+    />
+  </Icon>
+);
+
+IconPawa6.displayName = 'IconPawa6';
